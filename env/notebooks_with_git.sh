6aa4975d3d9fc57c1664e00f4fd75a042d7f7bef
abroniewski <abroniewski@gmail.com>
2022-09-15 16:22:07 +0200

    deleting support files used to clear notebook output when a commit is done.

--- env/notebooks_with_git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "\n Updating local git config files to clear jupyter notebook output on commit..."
-git config --local include.path ../env/.gitconfig
-printf "Done"
-
-printf "\n Renormalizing repository for changes to take effect...\n"
-git add --renormalize .
-printf "Done"
